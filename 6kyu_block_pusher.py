985d6e0f8aa69b6dd2f5eb49381444278e76a40e
Anatolii Olesh <sqanatoliy@gmail.com>
2023-09-25 18:56:21 +0300

    block pusher

---
+++ 6kyu_block_pusher.py
@@ -0,0 +1,26 @@
+def block_pushing(lst, n):
+    pusher = ">"
+    space = "-"
+
+    for move in range(n):
+        pusher_indexes = [index for index, value in enumerate(lst) if value == pusher]
+        for pusher_index in pusher_indexes:
+            try:
+                space_pos = pusher_index + lst[pusher_index:].index(space)
+
+                next_pusher = any(index > pusher_index and index < space_pos for index in pusher_indexes)
+                if next_pusher:
+                    next_pushers = [index for index, value in enumerate(lst[pusher_index+1:space_pos]) if value == pusher]
+                    for i in next_pushers:
+                        pusher_indexes.remove(pusher_index + 1 + i)
+                lst.pop(space_pos)
+                lst.insert(pusher_index, space)
+            except ValueError:
+                pass
+
+    return lst
+
+
+if __name__ == "__main__":
+    lst = ['#', '>', '#', '-', '-', '>', '>', '>', '-', '#', '-', '-']
+    block_pushing(lst, 2)
