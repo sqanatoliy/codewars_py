dbc1bb0675cb69546f9cf847a0218a38bbf8a48f
Anatolii Olesh <sqanatoliy@gmail.com>
2023-09-26 20:40:26 +0300

    refactored greeting

--- 7kyu/refactored_greeting.py
+++ 7kyu/refactored_greeting.py
@@ -1,27 +1,28 @@
 """DESCRIPTION:
-The following code could use a bit of object-oriented artistry. While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. (Or, at least, something similar depending on your language)
+The following code could use a bit of object-oriented artistry. 
+While it's a simple method and works just fine as it is, in a larger system it's best to organize methods 
+into classes/objects. (Or, at least, something similar depending on your language)
 
-Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
+Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. 
+The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
 
 Here is how the final refactored code would be used:
 
 joe = Person('Joe')
 joe.greet('Kate') # should return 'Hello Kate, my name is Joe'
 joe.name          # should == 'Joe'
-"""
-
-
 # TODO: This method needs to be called multiple times for the same person (my_name).
 # It would be nice if we didnt have to always pass in my_name every time we needed to great someone.
+
+"""
 
 class Person:
     def __init__(self, my_name) -> None:
         self.name = my_name
-        
-    
+
     def greet(self, your_name):
 
-        return "Hello %s, my name is %s" % (your_name, self.name)
+        return f"Hello {your_name}, my name is{self.name}"
     
 if __name__=="__main__":
     person = Person("Anatoliy")
